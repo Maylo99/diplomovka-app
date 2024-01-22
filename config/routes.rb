d841bb226930c4d900575240330f6ba5087b5a29
Michal Hájek <hajek.michal55@gmail.com>
2024-01-22 13:49:48 +0100

    fix: removed test posts model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   post "/", to: "posts#index"
-   root "posts#index"
+   root "artciles#index"
 end
