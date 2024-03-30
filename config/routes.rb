354aa4eed81f1ba6377e0ace76c3625afa8e070d
Michal Hájek <hajek.michal55@gmail.com>
2024-03-30 19:24:13 +0100

    delete show accounts show action

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :accounts, param: :account_id do
+  resources :accounts, param: :account_id, except: :show do
     member do
       resources :expenses
       resources :bank_accounts, except: :show
