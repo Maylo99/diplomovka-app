e3ef7e85fa6d61ee7ff7a1f504151b616a6f5e1d
Michal Hájek <hajek.michal55@gmail.com>
2024-04-10 19:59:16 +0200

    feat: automatic accounting fo expenses

--- app/models/expense_item.rb
+++ app/models/expense_item.rb
@@ -1,3 +1,7 @@
 class ExpenseItem < ApplicationRecord
   belongs_to :expense
+  attribute :accounting_case
+  attribute :accounting_case_side
+  attribute :account_dph_side
+  attribute :account_invoice_side
 end
