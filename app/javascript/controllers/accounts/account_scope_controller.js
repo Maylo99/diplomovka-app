1c8f461fdab7a34ce2bf0816b06e8219ed91d0f9
Michal Hájek <hajek.michal55@gmail.com>
2024-05-02 13:36:57 +0200

    general fixes

--- app/javascript/controllers/accounts/account_scope_controller.js
+++ app/javascript/controllers/accounts/account_scope_controller.js
@@ -8,24 +8,24 @@ export default class extends Controller {
         const resources = ["sales_invoices","supplier_invoices","bank_statements", "expenses","income_expenses","charge_expenses","main_books"]
         const selectedAccountId = event.target.value;
         let new_path = "/";
-
         if (selectedAccountId !== "accounts/new") {
             const path = window.location.pathname;
             if (path === '/' || isNaN(path.split("/")[2])) {
                 new_path += "accounts"+`/${selectedAccountId}/sales_invoices`;
             } else {
                 let resource = path.split("/")[3]
-                if (resource && resources.includes(resource)) {
+                if (resource && resources.includes(resource)&& resource !=='accounts') {
                     new_path += "accounts/"+ selectedAccountId +"/"+ resource;
                 } else {
                     new_path = path.replace(/^\/[^/]+/, `/${selectedAccountId}`);
                 }
             }
-        } else {
-            new_path = "accounts/new";
+            this.anchorTarget.href = new_path;
+            this.anchorTarget.click();
+        }else{
+            window.location.replace(
+                "http://127.0.0.1:5000/accounts/new",
+            );
         }
-
-        this.anchorTarget.href = new_path;
-        this.anchorTarget.click();
     }
 }
